bfcaf2bc22f32fe3e3fe1caa933d8a2fefb0a59e
3873757 <dendir_fun@yahoo.com>
2016-04-18 15:32:01 -0500

    ok

--- TMchecker/src/main/java/edu/mum/jjs/service/CheckerService.java
+++ TMchecker/src/main/java/edu/mum/jjs/service/CheckerService.java
@@ -1,13 +1,28 @@
 package edu.mum.jjs.service;
 
+import java.util.List;
+
+import edu.mum.jjs.domain.Appointment;
+import edu.mum.jjs.domain.Checker;
 import edu.mum.jjs.domain.Status;
+import edu.mum.jjs.domain.Student;
+import edu.mum.jjs.domain.TimeSlot;
 
 public interface CheckerService   
  {
+	List<Checker> findAllChecker();
 
-	public Status  Appove();
-	public Status Reject();
+	void rejectAppointment(Appointment appointment);
+
+	void approveAppointment(Appointment appointment);
+	void penddingndAppointment(Appointment appointment);
+
+	void modifyAppointment(Appointment appointment, String appointId);
 		
+	List<TimeSlot> getAlltimeSlots();
+	
+
+	
 	   
 }
  
--- TMchecker/src/main/java/edu/mum/jjs/serviceimpl/CheckerServiceImpl.java
+++ TMchecker/src/main/java/edu/mum/jjs/serviceimpl/CheckerServiceImpl.java
@@ -1,32 +1,92 @@
 package edu.mum.jjs.serviceimpl;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import edu.mum.jjs.domain.Appointment;
+import edu.mum.jjs.domain.Checker;
 import edu.mum.jjs.domain.Status;
+import edu.mum.jjs.domain.TimeSlot;
 import edu.mum.jjs.repository.AppointmentRepository;
+import edu.mum.jjs.repository.CheckerRepository;
+import edu.mum.jjs.repository.StudentRepository;
+import edu.mum.jjs.repository.TimeSlotRepository;
 import edu.mum.jjs.service.CheckerService;
 
 @Service
 @Transactional
 public class CheckerServiceImpl implements CheckerService {
-
+	private Logger logger;
+	
 	@Autowired
 	AppointmentRepository appointmentRepository;
+	@Autowired
+	CheckerRepository checkerRepository;
+	//@Autowired
+	TimeSlotRepository timeSlotRepository;
+	//@Autowired
+	StudentRepository studentRepository;
 
 	@Override
-	public Status Reject() {
-		// TODO Auto-generated method stub
-		return Status.APPROVED;
+
+	public void modifyAppointment(Appointment appointment, String appointId) {
+		appointment.setAppointId(appointId);
+		logger.info("Appointment  modif appointment id: " + appointment.getAppointId());
+		Appointment appointmentManaged = (Appointment) appointmentRepository.findByAppointId(appointment.getAppointId());
+		appointmentRepository.saveAndFlush(appointment);
+		if (appointmentManaged != null) {
+			logger.info("Modify appointment ok");
+		} else
+			logger.info("Modify appointment NOT ok");
+	}
+
+
+	@Override
+	public void rejectAppointment(Appointment appointment) {
+
+		appointment.setStatus(Status.REJECTED);
+		appointmentRepository.saveAndFlush(appointment);
+
 	}
 
 	@Override
-	
-	public Status Appove() 
-	{		
-		//appointmentRepository.findBystartDate(("startDate")>= CURRENT_DATE);         	        
-		return Status.REJECTED;
+	public void approveAppointment(Appointment appointment) {
+		appointment.setStatus(Status.APPROVED);
+		appointmentRepository.saveAndFlush(appointment);
+
 	}
 	
-}
+	@Override
+	public void penddingndAppointment(Appointment appointment) {
+		appointment.setStatus(Status.PENDDING);
+		appointmentRepository.saveAndFlush(appointment);
+	}
+	
+
+	@Override
+	public List<Checker> findAllChecker() {
+		return checkerRepository.findAll();
+	}
+	
+
+	@Override
+	public List<TimeSlot> getAlltimeSlots() {
+		return timeSlotRepository.findAll();
+	}
+
+	
+
+	
+
+	
+
+
+	}
+
+	
+
+
+
